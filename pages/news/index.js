3baf1a7c1ca7d41960f5c45155fb11db3bc84acd
FTZrecords <ftzrecords@gmail.com>
2021-07-03 19:46:44 +0900

    add All

--- pages/news/index.js
+++ pages/news/index.js
@@ -40,7 +40,7 @@ export async function getServerSideProps(context) {
   // 記事jsonの作成
   const fs = require("fs");
   const postFilenameList = fs
-    .readdirSync(resolve("MD", categoryName), "utf-8")
+    .readdirSync(resolve("content", categoryName), "utf-8")
     .filter((file) => file.endsWith("md")); // ["first.md","second.md"]
 
   // const DIR = join(process.cwd(), "content", categoryName);
@@ -50,12 +50,12 @@ export async function getServerSideProps(context) {
 
   const postList = postFilenameList.map((postFilename) => {
     let raw = fs.readFileSync(
-      resolve(process.cwd(), "MD", categoryName, postFilename),
+      resolve(process.cwd(), "content", categoryName, postFilename),
       "utf8"
     );
     let frontMatter = grayMatter(raw); // { content:"本文", data: { title:"タイトル", published: 2020-07-13T00:00:00.000Z } }
     return {
-      url: join(categoryName, postFilename.slice(0, -3)),
+      url: join("content", categoryName, postFilename.slice(0, -3)),
       title: String(frontMatter.data.title),
       data: String(frontMatter.data.published),
       content: frontMatter.content,
