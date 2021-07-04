ad9056b840e0888ff96aa7a39eea34487355c8f4
FTZrecords <ftzrecords@gmail.com>
2021-07-05 00:21:04 +0900

    add All

--- pages/news/[post].js
+++ pages/news/[post].js
@@ -1,18 +1,65 @@
 import { join, resolve, basename } from "path";
 
 import fs from "fs";
+import Image from "next/image";
+import Link from "next/link";
+
+import Markdown from "react-markdown";
 import grayMatter from "gray-matter";
+import remarkGfm from "remark-gfm";
+import unwrapImages from "remark-unwrap-images";
+
+//タイトルタグにジャンプリンクを
+import remarkSlug from "remark-slug";
+import remarkAutolinkHeadings from "remark-autolink-headings";
 
 import Layout from "components/Layout";
 import { formatDate } from "lib/date";
 
 const categoryName = "news";
 
+const renderers = {
+  image: (image) => {
+    return <Image src={image.src} alt={image.alt} height="200" width="355" />;
+  },
+};
+
 export default function Post({ title, data, content }) {
   return (
     <Layout title={title}>
       <time>{data}</time>
-      <main>{content}</main>
+      <Markdown
+        children={content}
+        remarkPlugins={[
+          remarkGfm,
+          unwrapImages,
+          remarkSlug,
+          [remarkAutolinkHeadings, { behavior: "wrap" }],
+        ]}
+        skipHtml={true} //HTMLはそのまま反映されるように
+        components={{
+          img: (e) => {
+            return <Image src={e.src} alt={e.alt} height="200" width="355" />;
+          },
+          link: (e) => {
+            if (e.href.match("http")) {
+              return (
+                <a href={e.href} target="_blank" rel="noopener noreferrer">
+                  {children}
+                </a>
+              );
+            }
+            if (e.href.slice(0, 1) == "#") {
+              return <a href={e.href}>{e.children}</a>;
+            }
+            return (
+              <Link href={e.href}>
+                <a>{e.child}</a>
+              </Link>
+            );
+          },
+        }} //rendererからcomponentsに変わった
+      />
     </Layout>
   );
 }
